eb0a7a3660c1f00782509c2f836ce910bb38d9c1
Alexey-Sagaydak <alex230703@yandex.ru>
2024-06-24 14:35:42 +0700

    fixed task2

--- Task2/src/main.c
+++ Task2/src/main.c
@@ -2,26 +2,41 @@
 #define N 5
 
 void init_array(int* arr, int n);
-void print_reversed_array(int* arr, int n);
+void reverse_array(int* arr, int n);
+void print_array(int* arr, int n);
 
 int main() {
   int arr[N];
 
   init_array(arr, N);
-  print_reversed_array(arr, N);
+
+  printf("Ввод -> ");
+  print_array(arr, N);
+
+  reverse_array(arr, N);
+
+  printf("Вывод -> ");
+  print_array(arr, N);
 
   return 0;
 }
 
 void init_array(int* arr, int n) {
-  printf("Введите элементы массива (%d):\n", n);
   for (int i = 0; i < n; i++) {
-    scanf("%d", &arr[i]);
+    arr[i] = i + 1;
   }
 }
 
-void print_reversed_array(int* arr, int n) {
-  for (int i = n - 1; i >= 0; i--) {
+void reverse_array(int* arr, int n) {
+  for (int i = 0; i < n / 2; i++) {
+    int temp = arr[i];
+    arr[i] = arr[n - i - 1];
+    arr[n - i - 1] = temp;
+  }
+}
+
+void print_array(int* arr, int n) {
+  for (int i = 0; i < n; i++) {
     printf("%d ", arr[i]);
   }
   printf("\n");
